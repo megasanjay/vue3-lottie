7e7f51e521d8438c4476490a8c40836775eb73eb
Sanjay Soundarajan <sanjay.16@live.com>
2023-06-15 19:40:09 -0700

    docs: update all references

--- docus/app.config.ts
+++ docus/app.config.ts
@@ -3,8 +3,6 @@ export default defineAppConfig({
     title: 'Vue 3 Lottie',
     description:
       'A simple component library that will add support for Lottie animations in your Vue 3/Nuxt 3 applications.    ',
-    image:
-      'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'megasanjay',
       github: 'megasanjay/vue3-lottie',
@@ -27,6 +25,7 @@ export default defineAppConfig({
     },
     header: {
       logo: true,
+      title: 'Vue 3 Lottie',
       showLinkIcon: true,
       exclude: [],
       fluid: true,
